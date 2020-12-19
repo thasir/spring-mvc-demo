b88cf3efe70b85b9be397197e4de0d31729c95de
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-19 14:21:59 +0530

    instead of hard coding into the jsp file we input from student.class

--- spring-mvc-demo/src/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/springdemo/mvc/Student.java
@@ -1,11 +1,22 @@
 package springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	private String firstName;
 	private String lastName;
 	private String country;
+	private LinkedHashMap<String, String> countryOptions;
 
-	
+	public Student() {
+		countryOptions = new LinkedHashMap<>();
+		countryOptions.put("US", "America");
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -21,6 +32,7 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getCountry() {
 		return country;
 	}
@@ -29,5 +41,8 @@ public class Student {
 		this.country = country;
 	}
 
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
 
 }
