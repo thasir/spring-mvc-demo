c864851d4124455471028ffc14d8343f0e998ad3
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-19 14:05:06 +0530

    hard coded country

--- spring-mvc-demo/src/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/springdemo/mvc/Student.java
@@ -3,7 +3,9 @@ package springdemo.mvc;
 public class Student {
 	private String firstName;
 	private String lastName;
+	private String country;
 
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -19,5 +21,13 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
 
 }
