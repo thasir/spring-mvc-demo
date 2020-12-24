98325aa165ef9516cefcce01969f14425f544289
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2020-12-24 18:04:22 +0530

    spring MVC form validation-validating number raanges and regular expression
    Signed-off-by: Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>

--- spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer-confirmation.jsp
@@ -8,6 +8,10 @@
 <title>Customer Confirmation</title>
 </head>
 <body>
-The Customer is Confirmed : ${customer.firstName} ${customer.lastName}
+	The Customer is Confirmed : ${customer.firstName} ${customer.lastName}
+	<br>
+	<br> Free Passes: ${customer.freePasses}
+	<br>
+	<br> Postal Code: ${customer.postalCode}
 </body>
 </html>
--- spring-mvc-demo/src/springdemo/mvc/CustormerController.java
+++ spring-mvc-demo/src/springdemo/mvc/CustormerController.java
@@ -32,6 +32,8 @@ public class CustormerController {
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 		System.out.println("Last Name : |"+theCustomer.getLastName()+"|");
+		System.out.println("Binding Result :"+ theBindingResult);
+		System.out.println("\n\n\n");
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
